04088f597936e937665475715ac9ebfa77cf86f8
Kamil <Kamil>
2016-05-28 11:37:47 -0600

    Add Skins

---
+++ core/src/com/mygdx/game/MainMenu.java
@@ -0,0 +1,28 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class MainMenu extends ApplicationAdapter{
+
+	Stage stage;
+	
+	@Override
+	public void create() {
+		
+		super.create();
+	}
+	
+	@Override
+	public void render() {
+
+		super.render();
+	}
+	
+	@Override
+	public void dispose() {
+		
+		super.dispose();
+	}
+	
+}
